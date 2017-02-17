5ca15ce0723de6f82ebddedff5ea2c89d5e56fe8
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-02-17 13:49:27 -0600

    Test failure

--- src/test/kotlin/hm/binkley/labs/skratch/ShellTest.kt
+++ src/test/kotlin/hm/binkley/labs/skratch/ShellTest.kt
@@ -1,9 +1,19 @@
 package hm.binkley.labs.skratch
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import java.io.IOException
 
 internal class ShellTest {
     @Test
-    fun shouldRunAny() = assertEquals(0, Shell("java", "-version").runAny())
+    fun shouldRunAnyWithSuccess()
+            = assertEquals(0, Shell("java", "-version").runAny())
+
+    @Test
+    fun shouldRunAnyWithFailure() {
+        assertThrows<IOException>(IOException::class.java, {
+            Shell("no-such-program").runAny()
+        })
+    }
 }
