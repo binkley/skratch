263154778e9c992228dd83870fda2f4d405199b4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-03-11 10:47:12 -0600

    More automation

--- src/main/kotlin/hm/binkley/labs/skratch/bdd/funcs/BDD.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/bdd/funcs/BDD.kt
@@ -5,9 +5,9 @@ import hm.binkley.labs.skratch.bdd.funcs.BDD.Companion.QED
 import hm.binkley.labs.skratch.bdd.funcs.BDD.Companion.THEN
 import hm.binkley.labs.skratch.bdd.funcs.BDD.Companion.WHEN
 import hm.binkley.labs.skratch.bdd.funcs.BDD.Given
-import hm.binkley.labs.skratch.bdd.funcs.BDD.Qed
-import hm.binkley.labs.skratch.bdd.funcs.BDD.Then
-import hm.binkley.labs.skratch.bdd.funcs.BDD.When
+import hm.binkley.labs.skratch.bdd.funcs.BDD.Given.When
+import hm.binkley.labs.skratch.bdd.funcs.BDD.Given.When.Then
+import hm.binkley.labs.skratch.bdd.funcs.BDD.Given.When.Then.Qed
 
 fun main(args: Array<String>) {
     println(GIVEN `an apple`
@@ -17,7 +17,7 @@ fun main(args: Array<String>) {
 }
 
 infix fun Given.`an apple`(WHEN: When) = When()
-infix fun When.`it falls`(THEN: Then) = Then(GIVEN)
+infix fun When.`it falls`(THEN: Then) = Then()
 infix fun Then.`Newton thinks`(QED: Qed) = BDD(GIVEN, WHEN)
 
 inline fun whoami() = Throwable().stackTrace[1].methodName
@@ -25,13 +25,16 @@ inline fun whoami() = Throwable().stackTrace[1].methodName
 data class BDD(val GIVEN: String, val WHEN: String, val THEN: String = whoami()) {
     companion object {
         val GIVEN = Given()
-        val WHEN = When()
-        val THEN = Then("")
-        val QED = Qed()
+        val WHEN = GIVEN.When()
+        val THEN = WHEN.Then()
+        val QED = THEN.Qed()
     }
 
-    class Given
-    class When(val GIVEN: String = whoami())
-    class Then(val GIVEN: String, val WHEN: String = whoami())
-    class Qed
+    class Given {
+        inner class When(val GIVEN: String = whoami()) {
+            inner class Then(val GIVEN: String = this@When.GIVEN, val WHEN: String = whoami()) {
+                inner class Qed
+            }
+        }
+    }
 }
