f939c3d47c0ace5dd9f3e5394bf618a6be3b8671
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-03-14 17:26:58 -0400

    Strings BDD, like funcs

--- src/main/kotlin/hm/binkley/labs/skratch/bdd/strings/BDD.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/bdd/strings/BDD.kt
@@ -1,8 +1,20 @@
 package hm.binkley.labs.skratch.bdd.strings
 
+import hm.binkley.labs.skratch.bdd.Apple
+import hm.binkley.labs.skratch.bdd.Newton
 import hm.binkley.labs.skratch.bdd.strings.BDD.Companion.So
 
+
 fun main(args: Array<String>) {
+    var apple: Apple? = null
+    BDD["an apple"] = { apple = Apple(Newton(thinking = false)) }
+    BDD["it falls"] = { apple?.fall() }
+    BDD["Newton thinks"] = {
+        assert(apple?.physicist?.thinking ?: false) {
+            "Newton is sleeping"
+        }
+    }
+
     println(So
             GIVEN "an apple"
             WHEN "it falls"
@@ -11,8 +23,21 @@ fun main(args: Array<String>) {
 
 data class BDD constructor(
         val GIVEN: String, val WHEN: String, val THEN: String) {
+    init {
+        (actions[GIVEN] ?: throw IllegalArgumentException(
+                "No GIVEN defined: $GIVEN")).invoke()
+        (actions[WHEN] ?: throw IllegalArgumentException(
+                "No WHEN defined: $WHEN")).invoke()
+        (actions[THEN] ?: throw IllegalArgumentException(
+                "No THEN defined: $THEN")).invoke()
+    }
+
     companion object {
         val So = So()
+        private val actions = HashMap<String, () -> Unit>()
+        operator fun set(clause: String, action: () -> Unit): Unit {
+            actions[clause] = action
+        }
     }
 
     class So {
