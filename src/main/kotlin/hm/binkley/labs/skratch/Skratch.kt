81fb2603acf55a9541c1bca2bc5f68cc776e519b
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-03-14 18:31:42 -0400

    Main done with infix

--- src/main/kotlin/hm/binkley/labs/skratch/Skratch.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/Skratch.kt
@@ -1,41 +1,5 @@
 package hm.binkley.labs.skratch
 
-import hm.binkley.labs.skratch.Bar.Companion.bar
-
-class Bar {
-    infix fun foo(body: (b: Bar) -> Bar): Bar {
-        println("foo: " + this)
-        println("body: " + body)
-        return body(this)
-    }
-
-    infix fun foo(b: Bar): Bar {
-        println("foo: " + this)
-        println("b: " + b)
-        return b
-    }
-
-    infix fun bob(body: (b: Bar) -> Bar): Bar {
-        println("bob: " + this)
-        println("body: " + body)
-        return body(this)
-    }
-
-    companion object {
-        val bar = Bar()
-    }
-}
-
-infix fun Int.foo(b: Int) = this + b
-
 fun main(args: Array<String>) {
-    println(1 foo 2)
-
-    bar foo bar foo {
-        println("body: " + it)
-        it
-    } bob {
-        println("body: " + it)
-        it
-    }
+    println(::main)
 }
