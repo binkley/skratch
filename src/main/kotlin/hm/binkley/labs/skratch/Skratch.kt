f03bc6784243e2ebfd37208ea60fbbf6bda7357f
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-04-06 09:53:11 -0500

    Specialized map for functions

--- src/main/kotlin/hm/binkley/labs/skratch/Skratch.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/Skratch.kt
@@ -1,5 +1,27 @@
 package hm.binkley.labs.skratch
 
+class X(private val a: String) : CharSequence by a {
+    override fun toString() = a
+}
+
+interface Bob<T>
+typealias IntBobby = (Int, Int) -> Bob<Int>
+
+class Q : MutableMap<String, Any> by mutableMapOf() {
+    operator fun <T> set(key: String, x: (a: Int, b: Int) -> Bob<T>) {
+        this[key] = x as Any
+    }
+}
+
 fun main(args: Array<String>) {
     println(::main)
+    println(X("abc"))
+    val q = Q()
+    q["bob"] = { a, b ->
+        object : Bob<Int> {
+            override fun toString() = "Int Bob"
+        }
+    }
+    q["mary"] = "mary"
+    println("${q["mary"]}: ${(q["bob"] as IntBobby)(1, 2)}")
 }
