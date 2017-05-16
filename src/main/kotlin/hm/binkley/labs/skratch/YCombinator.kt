2e5f826598a4f4185b996a124f9ba61078a7bcf0
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-05-16 10:36:08 -0400

    Y combinator

---
+++ src/main/kotlin/hm/binkley/labs/skratch/YCombinator.kt
@@ -0,0 +1,17 @@
+package hm.binkley.labs.skratch
+
+import java.util.function.Function
+
+object YCombinator {
+    private interface Branch<F, T> : Function<Branch<F, T>, Function<F, T>>
+
+    fun <F, T> y() = Function<Function<Function<F, T>, Function<F, T>>, Function<F, T>> {
+        object : Branch<F, T> {
+            override fun apply(x: Branch<F, T>) = it.apply(
+                    Function { x.apply(x).apply(it) })
+        }.apply(object : Branch<F, T> {
+            override fun apply(x: Branch<F, T>) = it.apply(
+                    Function { x.apply(x).apply(it) })
+        })
+    }
+}
