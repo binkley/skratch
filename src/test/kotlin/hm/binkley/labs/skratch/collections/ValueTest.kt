a1e6728d9b856e7c2f47cf12fb22cecc563a28bc
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-10-12 10:32:03 -0500

    Fully test Value

--- src/test/kotlin/hm/binkley/labs/skratch/collections/ValueTest.kt
+++ src/test/kotlin/hm/binkley/labs/skratch/collections/ValueTest.kt
@@ -2,18 +2,51 @@ package hm.binkley.labs.skratch.collections
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import hm.binkley.labs.skratch.collections.Value.DatabaseValue
+import hm.binkley.labs.skratch.collections.Value.Nonce
+import hm.binkley.labs.skratch.collections.Value.RuleValue
 import org.junit.jupiter.api.Test
 
 internal class ValueTest {
+    private val layer = 0
+    private val key = "foo"
     private val database: Database = mock()
+    val value = DatabaseValue(database, "3")
 
     @Test
     fun shouldRemoveFromDatabase() {
-        val value = DatabaseValue(database, "3")
+        value.remove(layer, key)
 
-        value.remove(0, "foo")
+        verify(database).remove(layer, key)
+    }
 
-        verify(database).remove(0, "foo")
+    @Test
+    fun shouldRemoveWhenReplacedByNothing() {
+        value.replaceWith(layer, key, Nonce)
+
+        verify(database).remove(layer, key)
+    }
+
+    @Test
+    fun shouldRemoveWhenReplacedByRule() {
+        value.replaceWith(layer, key, RuleValue({ _, _ -> 3 }))
+
+        verify(database).remove(layer, key)
+    }
+
+    @Test
+    fun shouldIgnoreWhenReplacedByAnotherValue() {
+        val other = DatabaseValue(database, "4")
+        value.replaceWith(layer, key, other)
+
+        verifyZeroInteractions(database)
+    }
+
+    @Test
+    fun shouldAddToDatabase() {
+        value.add(layer, key)
+
+        verify(database).upsert(layer, key, value.value)
     }
 }
