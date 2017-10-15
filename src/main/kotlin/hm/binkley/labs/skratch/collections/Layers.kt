b54c2aad677fdee30df5436fb9fee3e576fba065
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-10-15 14:46:27 -0500

    Flesh out what layers may look like

--- src/main/kotlin/hm/binkley/labs/skratch/collections/Layers.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/collections/Layers.kt
@@ -1,3 +1,11 @@
 package hm.binkley.labs.skratch.collections
 
-class Layers
+class Layers(private val list: MutableList<ValueMap> = ArrayList())
+    : AbstractList<ValueMap>() {
+    override val size: Int
+        get() = list.size
+
+    override fun get(index: Int) = list[index]
+
+    operator fun get(key: String) = list.mapNotNull { it[key] }
+}
