b8efa12d32dbf6794f950cfedaf56a5524665f68
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-11-08 14:51:19 -0600

    Pull out classes

--- src/main/kotlin/hm/binkley/labs/skratch/math/Fib.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/math/Fib.kt
@@ -1,7 +1,5 @@
 package hm.binkley.labs.skratch.math
 
-import java.util.Objects
-
 fun main(args: Array<String>) {
     for (n in -6..6) {
         val fib = Fib(n)
@@ -11,9 +9,7 @@ fun main(args: Array<String>) {
     println()
 
     val a = (-6..6).map(::Fib).map(Fib::char)
-    val b = (-6..6).map(::Fib).map { fib ->
-        -fib.char() * fib.det()
-    }.reversed()
+    val b = (-6..6).map(::Fib).map { -it.char() * it.det() }.reversed()
 
     println(a)
     println(b)
@@ -37,7 +33,7 @@ fun main(args: Array<String>) {
     println()
 
     for (n in -1..3)
-        println("F(0)^$n = ${Fib.pow(n)}")
+        println("F(1)^$n = ${Fib.pow(n)}")
 }
 
 class Fib(val n: Int) {
@@ -72,89 +68,3 @@ class Fib(val n: Int) {
         fun pow(n: Int) = fib1.pow(n)
     }
 }
-
-data class Mat2(val a: Ratio, val b: Ratio, val c: Ratio, val d: Ratio) {
-    constructor(a: Long, b: Long, c: Long, d: Long)
-            : this(Ratio(a), Ratio(b), Ratio(c), Ratio(d))
-
-    operator fun times(that: Mat2) = Mat2(
-            a * that.a + b * that.c,
-            a * that.b + b * that.d,
-            c * that.a + d * that.c,
-            c * that.b + d * that.d)
-
-    operator fun times(that: Ratio)
-            = Mat2(a * that, b * that, c * that, d * that)
-
-    operator fun div(that: Mat2) = this * that.inv()
-
-    operator fun get(row: Int, col: Int) = when {
-        row == 0 && col == 0 -> a
-        row == 0 && col == 1 -> b
-        row == 1 && col == 0 -> c
-        row == 1 && col == 1 -> d
-        else -> throw IndexOutOfBoundsException("$row, $col")
-    }
-
-    fun det() = a * d - b * c
-
-    fun inv() = Mat2(d, -b, -c, a) * det().inv()
-
-    fun pow(n: Int) = Fib(n - 1)
-
-    override fun toString() = "[$a $b / $c $d]"
-}
-
-class Ratio(n: Long, d: Long) {
-    val n: Long
-    val d: Long
-
-    init {
-        val gcm = gcm(n, d)
-        this.n = n / gcm
-        this.d = d / gcm
-    }
-
-    constructor(n: Long) : this(n, 1)
-
-    operator fun unaryPlus() = this
-    operator fun unaryMinus() = Ratio(-n, d)
-
-    operator fun plus(that: Ratio)
-            = Ratio(n * that.d + that.n * d, d * that.d)
-
-    operator fun plus(that: Long) = this + Ratio(that)
-
-    operator fun minus(that: Ratio) = this + -that
-    operator fun minus(that: Long) = this + -that
-
-    operator fun times(that: Ratio) = Ratio(n * that.n, d * that.d)
-    operator fun times(that: Long) = this * Ratio(that)
-    operator fun div(that: Ratio) = this * that.inv()
-    operator fun div(that: Long) = this / Ratio(that)
-
-    fun inv() = Ratio(d, n)
-
-    override fun toString() = if (1L == d) n.toString() else "$n/$d"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Ratio
-
-        return n == other.n && d == other.d
-    }
-
-    override fun hashCode() = Objects.hash(n, d)
-
-    companion object {
-        private fun gcm(a: Long, b: Long): Long
-                = if (b == 0L) a else gcm(b, a % b)
-    }
-}
-
-operator fun Long.plus(that: Ratio) = Ratio(this) + that
-operator fun Long.minus(that: Ratio) = Ratio(this) - that
-operator fun Long.times(that: Ratio) = Ratio(this) * that
-operator fun Long.div(that: Ratio) = Ratio(this) / that
---
+++ src/main/kotlin/hm/binkley/labs/skratch/math/Mat2.kt
@@ -0,0 +1,41 @@
+package hm.binkley.labs.skratch.math
+
+data class Mat2(val a: Ratio, val b: Ratio, val c: Ratio, val d: Ratio) {
+    constructor(a: Long, b: Long, c: Long, d: Long)
+            : this(Ratio(a),
+            Ratio(b),
+            Ratio(c),
+            Ratio(d))
+
+    operator fun times(that: Mat2) = Mat2(
+            a * that.a + b * that.c,
+            a * that.b + b * that.d,
+            c * that.a + d * that.c,
+            c * that.b + d * that.d)
+
+    operator fun times(that: Ratio)
+            = Mat2(a * that, b * that, c * that,
+            d * that)
+
+    operator fun div(that: Mat2) = this * that.inv()
+
+    operator fun get(row: Int, col: Int) = when {
+        row == 0 && col == 0 -> a
+        row == 0 && col == 1 -> b
+        row == 1 && col == 0 -> c
+        row == 1 && col == 1 -> d
+        else -> throw IndexOutOfBoundsException("$row, $col")
+    }
+
+    fun det() = a * d - b * c
+
+    fun inv() = Mat2(d, -b, -c, a) * det().inv()
+
+    fun pow(n: Int) = Fib(n)
+
+    override fun toString() = if (I == this) "I" else "[$a $b / $c $d]"
+
+    companion object {
+        val I = Mat2(1, 0, 0, 1)
+    }
+}
