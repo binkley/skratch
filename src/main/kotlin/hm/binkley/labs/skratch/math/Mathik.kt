14cafde910e56ad4ff350e520baee7f742fbda60
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-11-14 08:54:29 -0600

    Cleaner

--- src/main/kotlin/hm/binkley/labs/skratch/math/Mathik.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/math/Mathik.kt
@@ -6,6 +6,7 @@ import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReaderBuilder
 import org.jline.terminal.TerminalBuilder
 import org.mariuszgromada.math.mxparser.Expression
+import org.mariuszgromada.math.mxparser.Function
 import java.io.PrintWriter
 import java.lang.Math as nativeMath
 
@@ -60,7 +61,9 @@ fun main(args: Array<String>) {
 
     println()
 
-    val s = org.mariuszgromada.math.mxparser.Function("s(n, x) = if( x >= 1, n, s(n+1, x + rUni(0,1) ) )")
+    val s = Function("""
+s(n, x) = if( x >= 1, n, s(n+1, x + rUni(0,1) ) )
+""")
     val e = Expression("avg( i, 1, 10000, s(0,0) )", s)
     println(e.calculate())
 
