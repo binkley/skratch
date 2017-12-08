2948a30fe557d848feae915bf771224601e2c056
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-12-08 12:09:18 -0600

    Puzzler

---
+++ src/main/kotlin/hm/binkley/labs/skratch/Puzzlers.kt
@@ -0,0 +1,17 @@
+package hm.binkley.labs.skratch
+
+fun printSign(i: Int) {
+    if (i < 0) {
+        "negative"
+    } else if (i > 0) {
+        "positive"
+    } else {
+        "zero"
+    }.let { println(it) }
+}
+
+fun main(args: Array<String>) {
+    printSign(-1)
+    printSign(0)
+    printSign(1)
+}
