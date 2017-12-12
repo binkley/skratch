46b13df6c7073e3d417b6f254141cc97bb89ad42
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2017-12-12 09:57:35 -0600

    Cleaner money

--- src/main/kotlin/hm/binkley/labs/skratch/money/Money.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/money/Money.kt
@@ -3,6 +3,15 @@ package hm.binkley.labs.skratch.money
 import java.math.BigDecimal
 
 data class Money(val currency: String, val amount: BigDecimal) {
+    constructor(currency: String, amount: Int)
+            : this(currency, BigDecimal(amount))
+
+    constructor(currency: String, amount: Long)
+            : this(currency, BigDecimal(amount))
+
+    constructor(currency: String, amount: Double)
+            : this(currency, BigDecimal(amount))
+
     infix fun at(exchange: CurrencyExchange) = Exchange(this, exchange)
 
     class Exchange(val money: Money, val exchange: CurrencyExchange) {
--- src/test/kotlin/hm/binkley/labs/skratch/money/MoneyTest.kt
+++ src/test/kotlin/hm/binkley/labs/skratch/money/MoneyTest.kt
@@ -2,19 +2,18 @@ package hm.binkley.labs.skratch.money
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 
 internal class MoneyTest {
     @Test
     fun convertNicely() {
-        val oneUSDollar = Money("USD", BigDecimal(1))
+        val oneUSDollar = Money("USD", 1)
         val exchange = object : CurrencyExchange {
             override fun exchange(money: Money, to: String)
-                    = if (to == "SGD") Money("SGD", BigDecimal(1.35))
+                    = if (to == "SGD") Money("SGD", 1.35)
             else throw UnsupportedOperationException()
         }
 
-        assertEquals(Money("SGD", BigDecimal(1.35)),
+        assertEquals(Money("SGD", 1.35),
                 oneUSDollar at exchange to "SGD")
     }
 }
