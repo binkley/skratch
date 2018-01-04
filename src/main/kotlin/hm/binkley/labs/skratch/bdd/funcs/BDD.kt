e34ae6925d4071294b6857207cc0573f0393395a
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-01-04 15:10:34 -0600

    Formatting

--- src/main/kotlin/hm/binkley/labs/skratch/bdd/funcs/BDD.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/bdd/funcs/BDD.kt
@@ -1,7 +1,9 @@
 package hm.binkley.labs.skratch.bdd.funcs
 
-data class Qed(val GIVEN: String, val WHEN: String,
-        val THEN: String = caller()) {
+data class Qed(
+        private val GIVEN: String,
+        private val WHEN: String,
+        private val THEN: String = caller()) {
     override fun toString() = "GIVEN $GIVEN WHEN $WHEN THEN $THEN"
 
     companion object {
