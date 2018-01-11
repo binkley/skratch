4bfb8feda84805ee318006b15293da1097f40237
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-01-11 09:37:29 -0600

    Test fix of oleksiyp/mockk#15

---
+++ src/test/kotlin/hm/binkley/labs/skratch/MixMockkAndMockito.kt
@@ -0,0 +1,30 @@
+package hm.binkley.labs.skratch
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class MixMockkAndMockito {
+    @Test
+    fun withMockk() {
+        val bob = mockk<Bob>()
+        every { bob.nothing() } returns 1
+
+        assertEquals(bob.nothing(), 1)
+    }
+
+    @Test
+    fun withMockito() {
+        val bob: Bob = mock()
+        whenever(bob.nothing()).thenReturn(2)
+
+        assertEquals(bob.nothing(), 2)
+    }
+
+    interface Bob {
+        fun nothing(): Int
+    }
+}
