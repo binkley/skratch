fee417205065d3efd63fe8fbc7e1cc6e72b0f424
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-02-12 07:33:15 -0600

    Runs in IDE, does not appear in SureFire

---
+++ src/test/kotlin/hm/binkley/labs/skratch/awaitility/AwaitilityTest.kt
@@ -0,0 +1,32 @@
+package hm.binkley.labs.skratch.awaitility
+
+import kotlinx.coroutines.experimental.async
+import org.awaitility.Awaitility.await
+import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import java.lang.Thread.sleep
+import java.util.concurrent.TimeUnit.SECONDS
+
+internal class AwaitilityTest {
+    var x = 0
+
+    @BeforeEach
+    fun setUp() {
+        x = 0
+    }
+
+    @Test
+    fun shouldWait() {
+        slow()
+        await().atMost(1L, SECONDS).until {
+            x == 1
+        }
+    }
+
+    private fun slow() {
+        async {
+            sleep(100L)
+            x = 1
+        }
+    }
+}
