97cb46e399ce1342515575d5d9d36b6423111863
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-12-16 13:48:45 -0600

    Broken

--- src/main/kotlin/hm/binkley/labs/skratch/math/shell.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/math/shell.kt
@@ -11,28 +11,80 @@ import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReaderBuilder
 import org.jline.reader.UserInterruptException
 import org.jline.terminal.TerminalBuilder
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
 import java.lang.System.err
 import kotlin.system.exitProcess
 
 private var DEBUG = false
 
-fun main(args: Array<String>) {
-    val cli = ArgParser(
-        programName = "math.shell",
-        skipExtraArguments = true,
+@Command(
+    description = ["@|math shell|@"],
+    mixinStandardHelpOptions = true,
+    name = "math.shell",
+    version = ["0-SNAPSHOT"],
+)
+class MathShell : Runnable {
+    override fun run() {}
+
+    @Option(
+        description = ["Enable debug output"],
+        names = ["-d", "--debug"],
     )
-    val debug by cli.option(
-        description = "Enable debug output",
-        fullName = "debug",
-        shortName = "d",
-        type = ArgType.Boolean,
-    ).default(false)
-    cli.parse(args)
+    var debug = false
 
-    DEBUG = debug
+    @Parameters(
+        description = ["Optional expression to evaluate"],
+        paramLabel = "EXPRESSION",
+    )
+    var expression = arrayOf<String>()
+}
 
-    if (args.isNotEmpty()) {
-        println(parseExpression(args.joinToString(" ")).evaluate())
+fun main(args: Array<String>) {
+    val cliExpression: String
+
+    if (true) {
+        val shell = MathShell()
+        val cli = CommandLine(shell)
+        val xxx = cli.execute(*args)
+
+        println("FIGURE THIS OUT: $xxx")
+
+        println("help? ${cli.isUsageHelpRequested}")
+        if (cli.isUsageHelpRequested) {
+            cli.usage(System.out)
+            return
+        }
+        println("version? ${cli.isVersionHelpRequested}")
+        if (cli.isVersionHelpRequested) {
+            cli.printVersionHelp(System.out)
+            return
+        }
+
+        println("DID NOT EXIT")
+        DEBUG = shell.debug
+        cliExpression = shell.expression.joinToString(" ")
+    } else {
+        val cli = ArgParser(
+            programName = "math.shell",
+            skipExtraArguments = true,
+        )
+        val debug by cli.option(
+            description = "Enable debug output",
+            fullName = "debug",
+            shortName = "d",
+            type = ArgType.Boolean,
+        ).default(false)
+        cli.parse(args)
+
+        DEBUG = debug
+        cliExpression = args.joinToString(" ")
+    }
+
+    if (cliExpression.isNotEmpty()) {
+        println(parseExpression(cliExpression).evaluate())
         exitProcess(0)
     }
 
