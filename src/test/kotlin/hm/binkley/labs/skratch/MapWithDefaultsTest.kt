c607346d633b472b3fe97401398ae8562ee802d4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-12-17 07:50:55 -0600

    Late init is for newbs

--- src/test/kotlin/hm/binkley/labs/skratch/MapWithDefaultsTest.kt
+++ src/test/kotlin/hm/binkley/labs/skratch/MapWithDefaultsTest.kt
@@ -1,18 +1,18 @@
 package hm.binkley.labs.skratch
 
-import org.junit.Before
+import org.junit.After
 import org.junit.Test
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.test.assertEquals
 
 class MapWithDefaultsTest {
-    private lateinit var map: MutableMap<Int, Int>
+    private val map = object : ConcurrentHashMap<Int, Int>() {
+        override fun get(key: Int) = super.get(key) ?: 0
+    }
 
-    @Before
-    fun setUp() {
-        map = object : ConcurrentHashMap<Int, Int>() {
-            override fun get(key: Int) = super.get(key) ?: 0
-        }
+    @After
+    fun tearDown() {
+        map.clear()
     }
 
     @Test
