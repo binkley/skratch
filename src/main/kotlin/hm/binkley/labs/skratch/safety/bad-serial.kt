7a3499c17d860d414bec5a4fe6d07cb8d3c146f3
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2021-01-16 11:52:08 +0800

    Style

--- src/main/kotlin/hm/binkley/labs/skratch/safety/bad-serial.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/safety/bad-serial.kt
@@ -4,7 +4,9 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
+import java.io.PrintStream
 import java.io.Serializable
+import java.lang.System.out
 
 fun main() {
     val buf = ByteArrayOutputStream()
@@ -16,26 +18,24 @@ fun main() {
     serialOut.writeObject(safe)
 
     val data = buf.toByteArray()
-    val pretty = data.joinToString(" ", "[", "]") { "\\x%02X".format(it) }
-    println("BYTES -> $pretty")
 
-    for(i in data.indices) {
-        when (data[i]) {
-            'O'.toByte() -> {
-                println("O@$i => N")
-                data[i] = 'N'.toByte()
-            }
-            'K'.toByte() -> {
-                println("K@$i => O")
-                data[i] = 'O'.toByte()
-            }
-        }
-    }
+    prettyPrint(out, data)
 
     val serialIn = ObjectInputStream(ByteArrayInputStream(data))
     val wonky = serialIn.readObject() as Foo
 
     println("WONKY -> $wonky")
+}
+
+private fun prettyPrint(out: PrintStream, data: ByteArray) {
+    out.println(data.joinToString(" ", "[", "]") {
+        "\\x%02X".format(it)
+    })
+
+    for (i in data.indices) when (data[i]) {
+        'O'.toByte() -> data[i] = 'N'.toByte()
+        'K'.toByte() -> data[i] = 'O'.toByte()
+    }
 }
 
 data class Foo(val s: String) : Serializable {
