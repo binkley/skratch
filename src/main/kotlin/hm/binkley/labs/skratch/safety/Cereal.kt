6b259b07054d1a8c1d9eb1ec3e4ed8e04a2387ba
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2021-01-22 06:31:08 +0800

    Promoted

--- src/main/kotlin/hm/binkley/labs/skratch/safety/Cereal.kt
+++
@@ -1,8 +0,0 @@
-package hm.binkley.labs.skratch.safety
-
-data class Cereal(
-    val string: String,
-    val int: Int,
-    val byte: Byte,
-    val d: Double?,
-)
--- src/main/kotlin/hm/binkley/labs/skratch/safety/formatting.kt
+++
@@ -1,6 +0,0 @@
-package hm.binkley.labs.skratch.safety
-
-internal fun Byte.pretty() = "\\x%02x".format(this)
-internal fun ByteArray.pretty() = joinToString(" ", "[", "]") {
-    it.pretty()
-}
