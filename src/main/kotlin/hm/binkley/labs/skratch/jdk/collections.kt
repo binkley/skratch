683039f9da7eb64c727963ffd72fcb0ccd90c3b1
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2021-01-27 11:00:44 +0800

    NB: Lots of the JDK "just works"

---
+++ src/main/kotlin/hm/binkley/labs/skratch/jdk/collections.kt
@@ -0,0 +1,26 @@
+package hm.binkley.labs.skratch.jdk
+
+import java.util.concurrent.ConcurrentNavigableMap
+import java.util.concurrent.ConcurrentSkipListMap
+
+private fun Int.toAlphaLowerCase() = ('a'.toShort() + (this - 1)).toChar()
+
+fun main() {
+    val x: ConcurrentNavigableMap<String, Int> =
+        object : ConcurrentSkipListMap<String, Int>(
+            (1..10).map {
+                it.toAlphaLowerCase().toString() to it
+            }.shuffled().toMap()
+        ) {
+            override fun put(key: String, value: Int): Int? {
+                val old = super.put(key, value)
+                println("PUT -> $key: $value -> $old")
+                return old
+            }
+        }
+
+    println("MAP -> $x")
+    println("HEAD-MAP -> ${x.headMap("f")}")
+    println("TAIL-MAP -> ${x.tailMap("f")}")
+    println("SUB-MAP -> ${x.subMap("c", "i")}")
+}
