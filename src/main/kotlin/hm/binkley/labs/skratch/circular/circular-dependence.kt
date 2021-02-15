b95ab9bdfc1d5d32738a80ddb182ed4487f9be2e
Brian Oxley <boxley@thoughtworks.com>
2021-02-15 09:41:26 +0800

    Circularity

--- src/main/kotlin/hm/binkley/labs/skratch/circular/circular-dependence.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/circular/circular-dependence.kt
@@ -1,19 +1,43 @@
 package hm.binkley.labs.skratch.circular
 
 fun main() {
-    println(Q.T)
+    println("${P.ZERO} -> ${P.valueOf(0)}")
+    println("${Q.ZERO} -> ${Q.valueOf(0)}")
 }
 
-private abstract class XCompanion<T : X<T>>() {
-    abstract val T: T
+private interface Zeroable<T> {
+    val ZERO: T
 }
 
-private abstract class X<T : X<T>>() {
-    override fun toString() = "I am an X"
+private abstract class XCompanionBase<T : XBase<T>>(
+    @JvmField
+    override val ZERO: T,
+) : Zeroable<T> {
+    abstract fun valueOf(x: Int): T
+
+    protected fun construct(x: Int, ctor: (Int) -> T) =
+        if (0 == x) ZERO else ctor(x)
 }
 
-private class Q : X<Q>() {
-    companion object : XCompanion<Q>() {
-        override val T = Q()
+private abstract class XBase<T : XBase<T>> protected constructor(
+    val x: Int,
+    val companion: XCompanionBase<T>,
+) {
+    override fun toString() = "${super.toString()}: $x"
+}
+
+private class P private constructor(x: Int) : XBase<P>(x, Companion) {
+    companion object : XCompanionBase<P>(
+        ZERO = P(0),
+    ) {
+        override fun valueOf(x: Int): P = construct(x) { P(it) }
+    }
+}
+
+private class Q private constructor(x: Int) : XBase<Q>(x, Companion) {
+    companion object : XCompanionBase<Q>(
+        ZERO = Q(0),
+    ) {
+        override fun valueOf(x: Int): Q = construct(x) { Q(it) }
     }
 }
