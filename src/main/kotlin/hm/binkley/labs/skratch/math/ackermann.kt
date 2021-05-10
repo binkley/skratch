4ad52b2802526a1617de8f5ec70bda14d83271fa
Brian Oxley <boxley@thoughtworks.com>
2021-05-10 14:27:20 +0800

    Ackermann's function

---
+++ src/main/kotlin/hm/binkley/labs/skratch/math/ackermann.kt
@@ -0,0 +1,27 @@
+package hm.binkley.labs.skratch.math
+
+fun main() {
+    for (m in 1uL..3uL) // Stack overflow with 4
+        for (n in 1uL..12uL) // Stack overflow with 13
+            println("$m,$n -> ${ackermann(m, n)}")
+}
+
+private fun ackermann0(m: ULong, n: ULong): ULong {
+    return when {
+        0uL == m -> n + 1uL
+        0uL == n -> ackermann(m - 1uL, 1uL)
+        else -> {
+            ackermann(m - 1uL, ackermann(m, n - 1uL))
+        }
+    }
+}
+
+private tailrec fun ackermann(m: ULong, n: ULong): ULong {
+    return when {
+        0uL == m -> n + 1uL
+        0uL == n -> ackermann(m - 1uL, 1uL)
+        else -> {
+            ackermann(m - 1uL, ackermann0(m, n - 1uL))
+        }
+    }
+}
