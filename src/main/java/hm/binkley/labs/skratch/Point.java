fd06401e9b32fdc51508715bfe1e0ed4c963e4c3
Brian Oxley <boxley@thoughtworks.com>
2021-05-13 05:32:13 +0800

    Run Java classes as well

--- src/main/java/hm/binkley/labs/skratch/Point.java
+++ src/main/java/hm/binkley/labs/skratch/Point.java
@@ -1,3 +1,9 @@
 package hm.binkley.labs.skratch;
 
-record Point(int x, int y) { }
+import static java.lang.System.out;
+
+record Point(int x, int y) {
+    public static void main(final String... args) {
+        out.printf("POINT -> %s%n", new Point(1, 2));
+    }
+}
