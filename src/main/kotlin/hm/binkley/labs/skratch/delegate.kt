021ca3d6b712b564ffc8a68987a63b2737923a66
Brian Oxley <boxley@thoughtworks.com>
2021-05-13 05:25:20 +0800

    Layout

--- src/main/kotlin/hm/binkley/labs/skratch/delegate.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/delegate.kt
@@ -1,12 +1,6 @@
 package hm.binkley.labs.skratch
 
 fun main() {
-    data class Foo(private val map: MutableMap<String, Any?>) :
-        MutableMap<String, Any?> by map {
-        val name: String by this
-        val number: Int by this
-    }
-
     val foo = Foo(mutableMapOf(
         "name" to "BOB",
         "number" to 42,
@@ -16,4 +10,10 @@ fun main() {
     println("AGE -> ${foo.name}")
     println("NUMBER -> ${foo.number}")
     println("PI -> ${foo["pi"]}")
+}
+
+private data class Foo(private val map: MutableMap<String, Any?>) :
+    MutableMap<String, Any?> by map {
+    val name: String by this
+    val number: Int by this
 }
