a3e401dc36eac6547e452b2ed6c5a5965cff0328
Brian Oxley <boxley@thoughtworks.com>
2021-05-27 14:56:15 +0800

    Still not right, but less wrong

--- src/main/kotlin/hm/binkley/labs/skratch/layers/Layers.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/layers/Layers.kt
@@ -8,35 +8,70 @@ import kotlin.collections.Map.Entry
 fun main() {
     val c = defaultMutableLayers<String, Number>()
     c.edit {
-        this["ALICE"] = P(3)
+        this["ALICE"] =
+            Rule.rule<String, Number, Int>("Latest of") { _, values, _ ->
+                if (values.isEmpty()) 0 else values.last()
+            }
+        this["BOB"] =
+            Rule.rule<String, Number, Double>("Sum[Int]") { _, values, _ ->
+                values.sum()
+            }
+    }
+    c.commitAndNext()
+    c.edit {
+        this["ALICE"] = Value(3)
     }
     val a = c.commitAndNext()
     a.edit {
-        this["BOB"] = P(4.0)
+        this["BOB"] = Value(4.0)
     }
-    println(c)
     println(c.history)
+    println(c)
 }
 
-data class P<V : Any>(val value: V)
+sealed interface ValueOrRule<V : Any>
+data class Value<V : Any>(val value: V) : ValueOrRule<V> {
+    override fun toString() = "<Value>:$value"
+}
 
-interface Layer<K : Any, V : Any, L : Layer<K, V, L>> : Map<K, P<V>> {
+abstract class Rule<K : Any, V : Any, T : V>(
+    val name: String,
+) : ValueOrRule<V>,
+        (K, List<T>, Layers<K, V, *>) -> T {
+    override fun toString() = "<Rule>:$name"
+
+    companion object {
+        fun <K : Any, V : Any, T : V> rule(
+            name: String,
+            block: (K, List<T>, Layers<K, V, *>) -> T,
+        ): Rule<K, V, T> = object : Rule<K, V, T>(name) {
+            override fun invoke(
+                key: K,
+                values: List<T>,
+                layers: Layers<K, V, *>,
+            ): T = block(key, values, layers)
+        }
+    }
+}
+
+interface Layer<K : Any, V : Any, L : Layer<K, V, L>> :
+    Map<K, ValueOrRule<V>> {
     @Suppress("UNCHECKED_CAST")
     val self: L
         get() = this as L
 }
 
-interface MutableLayer<K : Any, V : Any, M : Layer<K, V, M>> :
+interface MutableLayer<K : Any, V : Any, M : MutableLayer<K, V, M>> :
     Layer<K, V, M>,
-    MutableMap<K, P<V>> {
-
-    fun edit(block: MutableMap<K, P<V>>.() -> Unit)
+    MutableMap<K, ValueOrRule<V>> {
+    fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit)
 }
 
 open class DefaultMutableLayer<K : Any, V : Any, M : DefaultMutableLayer<K, V, M>>(
-    val map: MutableMap<K, P<V>> = mutableMapOf(),
-) : MutableLayer<K, V, M>, MutableMap<K, P<V>> by map {
-    override fun edit(block: MutableMap<K, P<V>>.() -> Unit) = map.block()
+    val map: MutableMap<K, ValueOrRule<V>> = mutableMapOf(),
+) : MutableLayer<K, V, M>, MutableMap<K, ValueOrRule<V>> by map {
+    override fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit) =
+        map.block()
 
     override fun toString(): String = map.toString()
 
@@ -49,23 +84,23 @@ open class DefaultMutableLayer<K : Any, V : Any, M : DefaultMutableLayer<K, V, M
 }
 
 interface Layers<K : Any, V : Any, L : Layer<K, V, L>> : Map<K, V> {
-    val history: List<Map<K, P<V>>>
+    val history: List<Map<K, ValueOrRule<V>>>
 }
 
 interface MutableLayers<K : Any, V : Any, M : MutableLayer<K, V, M>>
     : Layers<K, V, M> {
-    fun edit(block: MutableMap<K, P<V>>.() -> Unit)
+    fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit)
 
-    /** @todo How to return M instead? */
     fun commitAndNext(): MutableLayer<K, V, M>
+    fun <N : M> commitAndNext(next: () -> N): N
 }
 
 open class DefaultMutableLayers<K : Any, V : Any, M : MutableLayer<K, V, M>>(
-    private val list: MutableList<M> = mutableListOf(),
-    private val newA: () -> M,
+    private val layers: MutableList<M> = mutableListOf(),
+    private val defaultMutableLayer: () -> M,
 ) : MutableLayers<K, V, M>, AbstractMap<K, V>() {
     init {
-        if (list.isEmpty()) list.add(newA())
+        if (layers.isEmpty()) layers.add(defaultMutableLayer())
     }
 
     companion object {
@@ -75,28 +110,26 @@ open class DefaultMutableLayers<K : Any, V : Any, M : MutableLayer<K, V, M>>(
             }
     }
 
-    override val history: List<Map<K, P<V>>> = list
+    override val history: List<Map<K, ValueOrRule<V>>> = layers
 
-    override fun edit(block: MutableMap<K, P<V>>.() -> Unit) =
-        list.last().block()
+    override fun edit(block: MutableMap<K, ValueOrRule<V>>.() -> Unit) =
+        layers.last().block()
 
-    override fun commitAndNext(): M {
-        val a = newA()
-        list.add(a)
-        return a
+    override fun commitAndNext(): M = commitAndNext(defaultMutableLayer)
+
+    override fun <N : M> commitAndNext(next: () -> N): N {
+        val layer = next()
+        layers.add(layer)
+        return layer
     }
-
-    private fun keys(): Set<K> = history.flatMap { it.keys }.toSet()
-    private fun computeValue(key: K): V =
-        history.first { it.containsKey(key) }[key]!!.value
 
     override val entries: Set<Entry<K, V>>
         get() = object : AbstractSet<Entry<K, V>>() {
-            override val size: Int get() = keys().size
+            override val size: Int get() = allKeys().size
 
             override fun iterator(): Iterator<Entry<K, V>> =
                 object : Iterator<Entry<K, V>> {
-                    val kit = keys().iterator()
+                    val kit = allKeys().iterator()
                     override fun hasNext(): Boolean = kit.hasNext()
 
                     override fun next(): Entry<K, V> {
@@ -105,4 +138,21 @@ open class DefaultMutableLayers<K : Any, V : Any, M : MutableLayer<K, V, M>>(
                     }
                 }
         }
+
+    private fun allKeys(): Set<K> = history.flatMap { it.keys }.toSet()
+    private fun computeValue(key: K): V {
+        val rule = currentRuleFor<V>(key)
+        val values = currentValuesFor<V>(key)
+
+        return rule(key, values, this)
+    }
+
+    private fun <T : V> currentRuleFor(key: K): Rule<K, V, T> =
+        valuesOrRules(key).filterIsInstance<Rule<K, V, T>>().last()
+
+    private fun <T : V> currentValuesFor(key: K): List<T> =
+        valuesOrRules(key).filterIsInstance<Value<T>>().map { it.value }
+
+    private fun valuesOrRules(key: K): List<ValueOrRule<V>> =
+        layers.mapNotNull { it[key] }
 }
