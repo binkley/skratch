e3a51faeb315a8fb969832d141ce0de3b6777610
Brian Oxley <boxley@thoughtworks.com>
2021-07-08 08:47:14 +0800

    More generics redux

--- src/main/kotlin/hm/binkley/labs/skratch/generics/Base.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/generics/Base.kt
@@ -10,19 +10,19 @@ interface EditMap<K : Any, V : Any> : MutableMap<K, V>
 
 interface MutableBase<K : Any, V : Any, M : MutableBase<K, V, M>>
     : Base<K, V, M> {
-    fun mutate(block: EditMap<K, V>.() -> Unit): M
+    fun edit(block: EditMap<K, V>.() -> Unit): M
 }
 
 abstract class AbstractBase<K : Any, out V : Any, out B : AbstractBase<K, V, B>>(
-    protected open val map: Map<K, V>,
+    private val map: Map<K, V>,
 ) : Base<K, V, B>, Map<K, V> by map {
     override fun toString() = map.toString()
 }
 
 abstract class AbstractMutableBase<K : Any, V : Any, M : AbstractMutableBase<K, V, M>>(
-    override val map: MutableMap<K, V>,
+    private val map: MutableMap<K, V>,
 ) : MutableBase<K, V, M>, AbstractBase<K, V, M>(map) {
-    override fun mutate(block: EditMap<K, V>.() -> Unit): M {
+    override fun edit(block: EditMap<K, V>.() -> Unit): M {
         DefaultEditMap().block()
         return self
     }
--- src/main/kotlin/hm/binkley/labs/skratch/generics/Bases.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/generics/Bases.kt
@@ -9,9 +9,15 @@ interface Bases<K : Any, out V : Any, out B : Base<K, V, B>>
 
 interface MutableBases<K : Any, V : Any, M : MutableBase<K, V, M>>
     : Bases<K, V, M> {
-    fun mutate(block: EditMap<K, V>.() -> Unit) = last().mutate(block)
+    fun edit(block: EditMap<K, V>.() -> Unit) = last().edit(block)
 
     fun <N : M> add(new: N): N
+    fun new(): M
+    fun add(block: EditMap<K, V>.() -> Unit): M {
+        val new = add(new())
+        edit(block)
+        return new
+    }
 }
 
 private fun <K : Any, V : Any, B : Base<K, V, B>> defaultView(
@@ -35,19 +41,65 @@ abstract class AbstractMutableBases<K : Any, V : Any, M : MutableBase<K, V, M>>(
 }
 
 fun main() {
-    class MyMutableBase(
+    open class MyMutableBase(
         map: MutableMap<String, Number> = mutableMapOf(),
     ) : AbstractMutableBase<String, Number, MyMutableBase>(map)
 
     val bases =
-        object : AbstractMutableBases<String, Number, MyMutableBase>() {}
+        object : AbstractMutableBases<String, Number, MyMutableBase>() {
+            override fun new() = MyMutableBase()
 
-    println(bases)
-    println(bases.history)
+            fun doHickey(): MyMutableBase =
+                MyMutableBase(mutableMapOf("HUM-HUM" to 2))
+        }
+
+    println("--- EMPTY")
+    println("BASES -> $bases")
+    println("HISTORY -> ${bases.history}")
+    println("STRING -> ${bases["STRING"]}")
+    println("--- ADD EMPTY")
     println(bases.add(MyMutableBase()).self)
-    println(bases)
-    println(bases.history)
+    println("BASES -> $bases")
+    println("HISTORY -> ${bases.history}")
+    println("STRING -> ${bases["STRING"]}")
+    println("--- ADD NOT EMPTY")
     println(bases.add(MyMutableBase(mutableMapOf("STRING" to 3))).self)
-    println(bases)
-    println(bases.history)
+    println("BASES -> $bases")
+    println("HISTORY -> ${bases.history}")
+    println("STRING -> ${bases["STRING"]}")
+    println("--- ADD CUSTOM")
+    println(bases.add(bases.doHickey()).self)
+    println("--- ADD VIA BLOCK")
+    println(bases.add {
+        this["BOB"] = 77
+    })
+    println("BASES -> $bases")
+    println("HISTORY -> ${bases.history}")
+    println("STRING -> ${bases["STRING"]}")
+
+    open class OhMyMutableBase<M : OhMyMutableBase<M>>
+        : MyMutableBase(mutableMapOf("message" to 17)) {
+        open fun ohMy() = println("OH, MY, ${this["message"]}!")
+    }
+
+    class MyWordMutableBase : OhMyMutableBase<MyWordMutableBase>() {
+        init {
+            edit {
+                this["message"] = 31
+            }
+        }
+
+        fun myWord() = println("MY, WORD!")
+    }
+
+    val wordy = bases.add(MyWordMutableBase()).apply {
+        ohMy()
+        myWord()
+    }
+
+    println("-- MORE EXTENDING")
+    println("WORDY -> $wordy")
+    println("BASES -> $bases")
+    println("HISTORY -> ${bases.history}")
+    println("STRING -> ${bases["STRING"]}")
 }
