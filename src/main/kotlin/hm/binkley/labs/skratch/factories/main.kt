08562dddca33e4379a1c74441a1c764136c0d63e
Brian Oxley <boxley@thoughtworks.com>
2022-03-03 19:03:01 -0600

    Runtime check not to mix systems of units
    Compile-time enforcement would be better.

--- src/main/kotlin/hm/binkley/labs/skratch/factories/main.kt
+++ src/main/kotlin/hm/binkley/labs/skratch/factories/main.kt
@@ -3,6 +3,7 @@ package hm.binkley.labs.skratch.factories
 import hm.binkley.labs.skratch.factories.Bar.Bars
 import hm.binkley.labs.skratch.factories.Baz.Bazs
 import hm.binkley.labs.skratch.factories.Foo.Foos
+import hm.binkley.labs.skratch.factories.Grok.Groks
 import java.util.Objects.hash
 
 fun main() {
@@ -36,33 +37,42 @@ fun main() {
     val remainders = foo14_2.into(Foos, Bazs, Bars)
     val sum4 = remainders.fold(0.foo) { acc, it -> acc + it }
     println("SUM #4 -> $sum4 $remainders")
+
+    val foo7 = 14.foo
+    println(foo7)
+    foo7.into(Bazs, Bars, Groks)
 }
 
 infix fun <
-        S : System<S>,
-        V : Units<S, V, N>,
-        N : Measure<S, V, N>>
+    S : System<S>,
+    V : Units<S, V, N>,
+    N : Measure<S, V, N>>
 Measure<*, *, *>.into(other: V): N = other.new(convertByBases(other) { it })
 
-abstract class System<S : System<S>>
+abstract class System<S : System<S>>(
+    val name: String
+) {
+    override fun toString() = name
+}
 
 abstract class Units<
-        S : System<S>,
-        U : Units<S, U, M>,
-        M : Measure<S, U, M>>(
+    S : System<S>,
+    U : Units<S, U, M>,
+    M : Measure<S, U, M>>(
+    val system: S,
     val name: String,
     val basis: BigRational,
 ) : Comparable<Units<*, *, *>> {
     abstract fun new(value: BigRational): M
     override fun compareTo(other: Units<*, *, *>) =
         basis.compareTo(other.basis)
-    override fun toString() = "$name@$basis"
+    override fun toString() = "$system $name@$basis"
 }
 
 abstract class Measure<
-        S : System<S>,
-        U : Units<S, U, M>,
-        M : Measure<S, U, M>>(
+    S : System<S>,
+    U : Units<S, U, M>,
+    M : Measure<S, U, M>>(
     val unit: U,
     val value: BigRational,
 ) {
@@ -80,12 +90,12 @@ abstract class Measure<
     override fun toString() = "$value $unit"
 }
 
-object Meta : System<Meta>()
+object Meta : System<Meta>("Meta")
 
 class Foo private constructor(
     value: BigRational,
 ) : Measure<Meta, Foos, Foo>(Foos, value) {
-    companion object Foos : Units<Meta, Foos, Foo>("foo", 1.rat) {
+    companion object Foos : Units<Meta, Foos, Foo>(Meta, "foo", 1.rat) {
         override fun new(value: BigRational) = Foo(value)
     }
 }
@@ -96,7 +106,7 @@ val Int.foo: Foo get() = rat.foo
 class Bar private constructor(
     value: BigRational,
 ) : Measure<Meta, Bars, Bar>(Bars, value) {
-    companion object Bars : Units<Meta, Bars, Bar>("bar", 3.rat) {
+    companion object Bars : Units<Meta, Bars, Bar>(Meta, "bar", 3.rat) {
         override fun new(value: BigRational) = Bar(value)
     }
 }
@@ -107,7 +117,7 @@ val Int.bar: Bar get() = rat.bar
 class Baz private constructor(
     value: BigRational,
 ) : Measure<Meta, Bazs, Baz>(Bazs, value) {
-    companion object Bazs : Units<Meta, Bazs, Baz>("baz", 9.rat) {
+    companion object Bazs : Units<Meta, Bazs, Baz>(Meta, "baz", 9.rat) {
         override fun new(value: BigRational) = Baz(value)
     }
 }
@@ -115,7 +125,28 @@ class Baz private constructor(
 val BigRational.baz: Baz get() = Bazs.new(this)
 val Int.baz: Baz get() = rat.baz
 
-fun Measure<*, *, *>.into(vararg units: Units<*, *, *>): List<Measure<*, *, *>> {
+object Martian : System<Martian>("Martian")
+
+class Grok private constructor(
+    value: BigRational,
+) : Measure<Martian, Groks, Grok>(Groks, value) {
+    companion object Groks :
+        Units<Martian, Groks, Grok>(Martian, "grok", 9.rat) {
+        override fun new(value: BigRational) = Grok(value)
+    }
+}
+
+fun Measure<*, *, *>.into(
+    vararg units: Units<*, *, *>
+): List<Measure<*, *, *>> {
+    // TODO: How to make this a compile-time error?
+    val system = unit.system
+    units.map { it.system }.forEach {
+        if (system != it) throw IllegalArgumentException(
+            "Units from different systems: expected $system but got $it"
+        )
+    }
+
     // Pre-populate with nulls so that we may write in any order
     val into = MutableList<Measure<*, *, *>?>(units.size) { null }
 
@@ -141,9 +172,6 @@ private fun Measure<*, *, *>.convertByBases(
 ) = conversion(value * unit.basis) / other.basis
 
 private fun <T : Comparable<T>> Array<T>.sortedDescendingIndexed() =
-    mapIndexed { index, it -> index to it }.sortedByDescending { it.second }
-
-private fun <T : Comparable<T>> List<T>.sortedDescendingIndexed() =
     mapIndexed { index, it -> index to it }.sortedByDescending { it.second }
 
 private fun <T> Collection<T?>.toNonNullableList() = map { it!! }
