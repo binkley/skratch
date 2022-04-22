2c1f7d7b1655033aee23a018b95ea00e381b107b
Brian Oxley <boxley@thoughtworks.com>
2022-04-22 07:04:35 -0500

    Testing

--- src/test/kotlin/hm/binkley/labs/skratch/layers/MutableLayerTest.kt
+++ src/test/kotlin/hm/binkley/labs/skratch/layers/MutableLayerTest.kt
@@ -15,7 +15,7 @@ internal class MutableLayerTest {
     }
 
     @Test
-    fun `should defensive copy`() {
+    fun `should defensive copy for constructor`() {
         val map = mutableMapOf("BOB" to 17.toValue())
         val layer = TestLayer(0, map)
 
@@ -26,6 +26,19 @@ internal class MutableLayerTest {
         layer shouldNotBe map
 
         map.clear()
+
+        layer shouldBe map
+    }
+
+    @Test
+    fun `should defensive copy for copy`() {
+        val map = mapOf("BOB" to 17.toValue())
+        val oldLayer = TestLayer(0, map)
+        val layer = oldLayer.copy()
+
+        layer shouldBe map
+
+        oldLayer.edit { clear() }
 
         layer shouldBe map
     }
